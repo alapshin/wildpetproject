fd7cb0a6fb896225eb740f40e84cb00164fd6398
Andrei Lapshin <alapshin@fastmail.com>
2023-06-19 21:55:32 +0200

    Fix navigation to another screen

--- shared/src/commonMain/kotlin/com/alapshin/multiplayground/login/view/LoginScreen.kt
+++ shared/src/commonMain/kotlin/com/alapshin/multiplayground/login/view/LoginScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,6 +31,12 @@ fun LoginScreen(bloc: LoginBloc) {
 
     var email by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+
+    if (state.success == true) {
+        LaunchedEffect(state) {
+            bloc.navigateToMain()
+        }
+    }
 
     Column(
         verticalArrangement = Arrangement.Center,
